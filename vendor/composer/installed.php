857e77ae24ea7f87b7ae6b85e4682c074e947ba5
makaravich <d.makarski@gmail.com>
2025-03-16 14:02:08 +0100

    Edits

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'makaravich/wptoptions',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'edbef9ffe985ff13cc550f9d4a9c01a2b330499f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'makaravich/wptoptions' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'edbef9ffe985ff13cc550f9d4a9c01a2b330499f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
