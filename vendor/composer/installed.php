032654108161badf13f8ed0fc5f814f33f47fcd8
dmakarki <dmitriy.m@one-v.co.il>
2025-06-15 13:03:34 +0200

    V 0.0.16

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'makaravich/wptoptions',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => '0.0.16',
+        'version' => '0.0.16.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'makaravich/wptoptions' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => '0.0.16',
+            'version' => '0.0.16.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
